6837d83fe533fff2529d9ac4ccf03ad4570422ff
Dmitry Shmidt <dima.shmidt@gmail.com>
2015-07-01 16:18:50 -0700

    Development pods

---
+++ Example/Pods/Target Support Files/Pods-GooglePlacesSearchController_Example/Pods-GooglePlacesSearchController_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GooglePlacesSearchController_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GooglePlacesSearchController_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-GooglePlacesSearchController_Tests-GooglePlacesSearchController/Pods-GooglePlacesSearchController_Tests-GooglePlacesSearchController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GooglePlacesSearchController_Tests_GooglePlacesSearchController : NSObject
+@end
+@implementation PodsDummy_Pods_GooglePlacesSearchController_Tests_GooglePlacesSearchController
+@end
