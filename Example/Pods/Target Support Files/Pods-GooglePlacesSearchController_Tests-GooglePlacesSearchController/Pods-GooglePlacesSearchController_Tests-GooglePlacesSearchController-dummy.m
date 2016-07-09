c72e15332e3825d1488f0191ed45e216e425aab8
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2016-07-09 21:15:38 +0300

    Update example project with Cocoapods 1.0.1

--- Example/Pods/Target Support Files/Pods-GooglePlacesSearchController_Tests-GooglePlacesSearchController/Pods-GooglePlacesSearchController_Tests-GooglePlacesSearchController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GooglePlacesSearchController_Tests_GooglePlacesSearchController : NSObject
-@end
-@implementation PodsDummy_Pods_GooglePlacesSearchController_Tests_GooglePlacesSearchController
-@end
--- Example/Pods/Target Support Files/Pods-GooglePlacesSearchController_Tests-GooglePlacesSearchController/Pods-GooglePlacesSearchController_Tests-GooglePlacesSearchController-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double GooglePlacesSearchControllerVersionNumber;
-FOUNDATION_EXPORT const unsigned char GooglePlacesSearchControllerVersionString[];
-
