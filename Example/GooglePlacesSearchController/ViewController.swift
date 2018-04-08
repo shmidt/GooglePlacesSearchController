dc4b88d9610140038e9be0cb20b0ada64d56f0a9
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2018-04-08 15:02:07 +0300

    Update example app

--- Example/GooglePlacesSearchController/ViewController.swift
+++ Example/GooglePlacesSearchController/ViewController.swift
@@ -14,29 +14,29 @@ import GooglePlacesSearchController
 class ViewController: UIViewController {
     let GoogleMapsAPIServerKey = "YOUR_KEY"
 
-    @IBAction func searchAddress(_ sender: UIBarButtonItem) {
-        
-        let controller = GooglePlacesSearchController(
-            apiKey: GoogleMapsAPIServerKey,
-            placeType: PlaceType.address
+    lazy var placesSearchController: GooglePlacesSearchController = {
+        let controller = GooglePlacesSearchController(delegate: self,
+                                                      apiKey: GoogleMapsAPIServerKey,
+                                                      placeType: .address
+            // Optional: coordinate: CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423),
+            // Optional: radius: 10,
+            // Optional: searchBarPlaceholder: "Start typing..."
         )
-        
-//        Or if you want to use autocompletion for specific coordinate and radius (in meters)        
-//        let coord = CLLocationCoordinate2D(latitude: 55.751244, longitude: 37.618423)
-//        let controller = GooglePlacesSearchController(
-//            apiKey: GoogleMapsAPIServerKey,
-//            placeType: PlaceType.Address,
-//            coordinate: coord,
-//            radius: 10
-//        )
-        
-        controller.didSelectGooglePlace { (place) -> Void in
-            print(place.description)
-            
-            //Dismiss Search
-            controller.isActive = false
-        }
-        
-        present(controller, animated: true, completion: nil)
+        //Optional: controller.searchBar.isTranslucent = false
+        //Optional: controller.searchBar.barStyle = .black
+        //Optional: controller.searchBar.tintColor = .white
+        //Optional: controller.searchBar.barTintColor = .black
+        return controller
+    }()
+
+    @IBAction func searchAddress(_ sender: UIBarButtonItem) {
+        present(placesSearchController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: GooglePlacesAutocompleteViewControllerDelegate {
+    func viewController(didAutocompleteWith place: PlaceDetails) {
+        print(place.description)
+        placesSearchController.isActive = false
     }
 }
